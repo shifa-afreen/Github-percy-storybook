c31b7144c60c2038ff8827d104a938af88c97407
somebodysmokein <venky88r@gmail.com>
2025-05-06 18:23:22 +0530

    updated the env variable inside a script

--- waitForBuildT0End.sh
+++ waitForBuildT0End.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # --- Configuration ---
-API_URL='https://percy.io/api/v1/builds/"$PERCY_BUILD_ID"'
+PERCY_BUILD_ID="$PERCY_BUILD_ID" 
+API_URL='https://percy.io/api/v1/builds/${PERCY_BUILD_ID}'
 # !! Replace 'xxx' with your actual Authorization token !!
 AUTH_TOKEN="$PERCY_TOKEN"
 echo "Token : $PERCY_TOKEN"
